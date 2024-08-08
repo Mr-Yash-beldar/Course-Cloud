c8784e1954f2e6bda12916a95ac1600a73521b9a
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-08-08 23:36:36 +0530

    database

--- include/config.php
+++ include/config.php
@@ -2,7 +2,7 @@
 defined('server') ? null : define("server", "localhost");
 defined('user') ? null : define("user", "root");
 defined('pass') ? null : define("pass", "");
-defined('database_name') ? null : define("database_name", "dbcaiwl");
+defined('database_name') ? null : define("database_name", "course_cloud");
 
 $this_file = str_replace('\\', '/', __FILE__);
 $doc_root = $_SERVER['DOCUMENT_ROOT'];
